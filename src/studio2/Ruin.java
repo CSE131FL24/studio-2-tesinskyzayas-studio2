a657c2ee5429efc1e8640ac20970e05be1f58f72
nenae <nenae@nenaslaptop.dhcp.wustl.edu>
2024-09-11 14:19:26 -0500

    Studio 2 Code

---
+++ src/studio2/Ruin.java
@@ -0,0 +1,27 @@
+package studio2;
+
+import java.util.Scanner;
+
+public class Ruin {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("How much money you starting with?");
+		double startAmount = in.nextDouble(); 
+		System.out.println("What's the probability of winning in a day?");
+		double winChance = in.nextDouble();
+		System.out.println("What's your winning limit?");
+		double winLimit = in.nextDouble(); 
+		 
+	
+		while (startAmount <= winLimit && startAmount >= 0) {
+			System.out.println(startAmount);
+			startAmount++;
+			
+		
+		if ()
+			
+		
+		}	
+	}
+}
